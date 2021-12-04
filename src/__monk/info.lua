b7ac9e429ffbbcf9723301006402fcce3ff172a1
cryi <cryi@tutanota.com>
2021-12-04 19:01:02 +0100

    fixed info, removed binaries dependency, added cli passthrough

--- src/__monk/info.lua
+++ src/__monk/info.lua
@@ -26,11 +26,11 @@ local _info = {
 
 local function _exec_monk_cli(...)
     local _arg = {'-datadir=data', ...}
-    -- local _rpcBind = am.app.get_configuration({'DAEMON_CONFIGURATION', 'rpcbind'})
-    -- if type(_rpcBind) == 'string' then
-    --     table.insert(_arg, 1, '-rpcconnect=' .. _rpcBind)
-    -- end
-    local _proc = proc.spawn('bin/monk-cli', _arg, {stdio = {stdout = 'pipe', stderr = 'pipe'}, wait = true})
+    local _rpcBind = am.app.get_configuration({'DAEMON_CONFIGURATION', 'rpcbind'})
+    if type(_rpcBind) == 'string' then
+        table.insert(_arg, 1, '-rpcconnect=' .. _rpcBind)
+    end
+    local _proc = proc.spawn(path.combine("bin", am.app.get_model("CLI_NAME")), _arg, {stdio = {stdout = 'pipe', stderr = 'pipe'}, wait = true})
 
     local _exitcode = _proc.exitcode
     local _stdout = _proc.stdoutStream:read('a') or ''
@@ -58,16 +58,17 @@ local function _get_monk_cli_result(exitcode, stdout, stderr)
 end
 
 if _info.monkd == 'running' then
-    -- if am.app.get_configuration("NODE_PRIVKEY") then
-    --     local _exitcode, _stdout, _stderr = _exec_monk_cli("-datadir=data", "masternode", "status")
-    --     local _success, _output = _get_monk_cli_result(_exitcode, _stdout, _stderr)
+    local _nodeType = "node"
+    if am.app.get_configuration("NODE_PRIVKEY") or am.app.get_configuration({"DAEMON_CONFIGURATION", "masternode"})  then
+        _nodeType = "masternode"
+        local _exitcode, _stdout, _stderr = _exec_monk_cli("-datadir=data", "getmasternodestatus")
+        local _success, _output = _get_monk_cli_result(_exitcode, _stdout, _stderr)
 
-    --     _info.status = _output.message
-    --     if not _success or (_info.status ~= 'Masternode successfully started') then
-    --         _info.level = "error"
-    --     end
-    -- end
-
+        _info.status = _output.message
+        if not _success or (_info.status ~= 'Masternode successfully started') then
+            _info.level = "error"
+        end
+    end
     local _exitcode, _stdout, _stderr = _exec_monk_cli('-datadir=data', 'getblockchaininfo')
     local _success, _output = _get_monk_cli_result(_exitcode, _stdout, _stderr)
 
@@ -83,17 +84,15 @@ if _info.monkd == 'running' then
         _info.synced = _output.IsBlockchainSynced
     end
 
-    if not _nodeType then
-        if not _success then
-            _info.status = 'Unknown sync status!'
-            _info.level = 'error'
-        else
-            if _info.synced then
-                _info.status = 'Synced.'
-            else
-                _info.status = 'Syncing'
-                _info.level = 'warn'
-            end
+    if not _success then
+        _info.status = 'Unknown sync status!'
+        _info.level = 'error'
+    else
+        if _info.synced and _nodeType ~= "masternode" then
+            _info.status = 'Synced.'
+        elseif not _info.synced then
+            _info.status = 'Syncing'
+            _info.level = 'warn'
         end
     end
 else
--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,16 +1,27 @@
 am.app.set_model(
     {
-        DAEMON_CONFIGURATION = {
-            server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
-            listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
-            masternodeprivkey = am.app.get_configuration("NODE_PRIVKEY"),
-            masternode = am.app.get_configuration("NODE_PRIVKEY") and 1 or nil
-        },
-        DAEMON_NAME = "monkd",
-        CLI_NAME = "monk-cli",
-        CONF_NAME = "monk.conf",
-        CONF_SOURCE = "__btc/assets/daemon.conf",
-        SERVICE_NAME = "monkd",
+        DAEMON_URL = 'https://github.com/decenomy/MONK/releases/download/v3.0.2.0/MONK-3.0.2.0-Linux.zip'
     },
-    { merge = true, overwrite = true }
+    {merge = true, overwrite = true}
+)
+
+am.app.set_model(
+    {
+        DAEMON_CONFIGURATION = {
+            server = (type(am.app.get_configuration('NODE_PRIVKEY') == 'string') or am.app.get_configuration('SERVER')) and
+                1 or
+                nil,
+            listen = (type(am.app.get_configuration('NODE_PRIVKEY') == 'string') or am.app.get_configuration('SERVER')) and
+                1 or
+                nil,
+            masternodeprivkey = am.app.get_configuration('NODE_PRIVKEY'),
+            masternode = am.app.get_configuration('NODE_PRIVKEY') and 1 or nil
+        },
+        DAEMON_NAME = 'monkd',
+        CLI_NAME = 'monk-cli',
+        CONF_NAME = 'monk.conf',
+        CONF_SOURCE = '__btc/assets/daemon.conf',
+        SERVICE_NAME = 'monkd'
+    },
+    {merge = true, overwrite = true}
 )
